33f98d5bbe97be8842d977146b6c7c13aa802018
thành hà <thanhht38@fpt.com>
2025-10-04 15:36:17 +0700

    update reponsive

--- index.js
+++ index.js
@@ -104,12 +104,3 @@
 document.addEventListener("contextmenu", function(e) {
     e.preventDefault();
 });
-
-document.addEventListener("keydown", function(e) {
-    if (e.key === "F12" || 
-        (e.ctrlKey && e.shiftKey && e.key === "I") || 
-        (e.ctrlKey && e.key === "U") || 
-        (e.ctrlKey && e.shiftKey && e.key === "J")) {
-        e.preventDefault();
-    }
-});
